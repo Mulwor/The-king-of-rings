24c8b4c35120d0053673b82fde1a6ee3c8e5a3c2
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-03-27 00:34:25 +0300

    refactor: change form card and list on function

--- src/components/Form/FormCard.tsx
+++ src/components/Form/FormCard.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { TFormList } from 'data/types';
 
-export class FormCard extends React.Component<TFormList> {
-  render() {
-    return (
-      <div className="ford">
-        <div className="wrapper">
-          <img className="avatar" src={this.props.photo} alt="фотография" />
-          <div>Your firstname: {this.props.firstname}</div>
-          <div>Your secondname: {this.props.secondname}</div>
-          <div>Your cmail: {this.props.email}</div>
-          <div>Your gender: {this.props.gender}</div>
-          <div>Your country: {this.props.country}</div>
-          <div>Your birthday: {this.props.date}</div>
-        </div>
+export function FormCard(props: TFormList) {
+  return (
+    <div className="ford">
+      <div className="wrapper">
+        <img className="avatar" src={props.photo} alt="фотография" />
+        <div>Your firstname: {props.firstname}</div>
+        <div>Your secondname: {props.secondname}</div>
+        <div>Your cmail: {props.email}</div>
+        <div>Your gender: {props.gender}</div>
+        <div>Your country: {props.country}</div>
+        <div>Your birthday: {props.date}</div>
       </div>
-    );
-  }
+    </div>
+  );
 }
--- src/components/Form/FormList.tsx
+++ src/components/Form/FormList.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import { TFormListProps, List } from 'data/types';
+import { List } from 'data/types';
 import { FormCard } from './FormCard';
 
-export class FormList extends React.Component<List, TFormListProps> {
-  constructor(props: List) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        {this.props.formList.map((list, i) => (
-          <FormCard
-            key={i}
-            firstname={list.firstname}
-            secondname={list.secondname}
-            email={list.email}
-            gender={list.gender}
-            country={list.country}
-            date={list.date}
-            photo={list.photo}
-          />
-        ))}
-      </div>
-    );
-  }
+export function FormList(props: List) {
+  return (
+    <div>
+      {props.formList.map((list, i) => (
+        <FormCard
+          key={i}
+          firstname={list.firstname}
+          secondname={list.secondname}
+          email={list.email}
+          gender={list.gender}
+          country={list.country}
+          date={list.date}
+          photo={list.photo}
+        />
+      ))}
+    </div>
+  );
 }
