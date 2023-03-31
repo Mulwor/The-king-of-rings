9e22b34f48d48ca9e5586273e3deece5e3f92a81
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-03-31 15:09:05 +0300

    feat: added unique token and output the values to the console

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,8 +2,25 @@ import React from 'react';
 import './index.css';
 import { Search } from '../../components/Search';
 import { Cards } from '../../components/Cards/Card';
+import axios from 'axios';
+
+const baseURL = 'https://the-one-api.dev/v2/character';
+const TOKEN = 'u828DLVp0wqOia5kQTP8';
 
 export function Home() {
+  React.useEffect(() => {
+    axios
+      .get(baseURL, {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          Accept: 'application/json',
+        },
+      })
+      .then((response) => {
+        console.log('Статус: ' + response.status, ' и данные, которые пришли', response.data.docs);
+      });
+  }, []);
+
   return (
     <div>
       <h2>Дом, милый дом!</h2>
