f1230c281e4733df727adf461093052f0e412644
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-04-03 14:54:18 +0300

    fix: modal window

--- src/components/Modal/Modals.tsx
+++ src/components/Modal/Modals.tsx
@@ -12,16 +12,25 @@ export interface CharacterItem {
 
 function Modals(item: CharacterItem) {
   return (
-    <div className="modal">
-      <button className="buttonela" onClick={item.onClose}>
-        X
-      </button>
-      <div>Name: {item.name}</div>
-      <div>Gender: {item.gender}</div>
-      <div>Race: {item.race}</div>
-      <div>Birth: {item.birth}</div>
-      <div>Depth: {item.death}</div>
-      <div>Wikipedia: {item.wikiUrl}</div>
+    <div
+      className="modalwrap"
+      onClick={(event) => {
+        if (event.target === event.currentTarget) {
+          item.onClose();
+        }
+      }}
+    >
+      <div className="modal">
+        <button className="buttonela" onClick={item.onClose}>
+          X
+        </button>
+        <div>Name: {item.name}</div>
+        <div>Gender: {item.gender}</div>
+        <div>Race: {item.race}</div>
+        <div>Birth: {item.birth}</div>
+        <div>Depth: {item.death}</div>
+        <div>Wikipedia: {item.wikiUrl}</div>
+      </div>
     </div>
   );
 }
